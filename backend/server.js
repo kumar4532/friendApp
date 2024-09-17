bbb8220796edea56230e58eacd9ef185b2aa1b89
kumar4532 <krantikumar2001@gmail.com>
2024-09-17 11:30:44 +0530

    deploy changes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -14,11 +15,18 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/request", requestRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.get("*",(req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 connectDB()
     .then(() => {
